b0b8378749ff4fff024edf1167c1d92678434b6b
tiendungtsptvn <tiendungtsptvn@gmail.com>
2022-12-19 17:55:45 +0700

    Remove code

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -5,24 +5,13 @@ import 'package:smart_audio/screens/playlist/playlist_screen.dart';
 import 'package:smart_audio/screens/search/search_screen.dart';
 import 'package:smart_audio/screens/tabs/tabs_screen.dart';
 import 'package:smart_audio/screens/wishlist_tracks/wishlist_screen.dart';
-import '../screens/change_password/change_password_screen.dart';
 import '../screens/chats/chats_screen.dart';
 import '../screens/chats/stored_conversation/stored_conversation_screen.dart';
-import '../screens/loading/loading_screen.dart';
-import '../screens/login/account_login_screen.dart';
-import '../screens/login/login_screen.dart';
+
 import 'router_name.dart';
 
 class Pages {
   static List<GetPage> pages = [
-    GetPage(name: RouterName.loading, transitionDuration: Duration.zero, page: () => LoadingScreen()),
-    GetPage(name: RouterName.login, page: () => const LoginScreen(), binding: LoginScreenBinding()),
-    GetPage(
-        name: RouterName.accountLogin, page: () => const AccountLoginScreen(), binding: AccountLoginScreenBinding()),
-    GetPage(
-        name: RouterName.changePassword,
-        page: () => const ChangePasswordScreen(),
-        binding: ChangePasswordScreenBinding()),
     GetPage(
       name: RouterName.chats,
       page: () => const ChatsScreen(),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:smart_audio/main_ui.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
