f893f825c46d9c9c41c37831fc0b00bc3499c8c0
tiendungtsptvn <tiendungtsptvn@gmail.com>
2022-12-25 23:39:15 +0700

    Update my playlist

--- lib/constants/app.dart
+++ lib/constants/app.dart
@@ -1,6 +1,6 @@
 /// Key for auth Service
-const String spotifyClientId = 'e608d11d834d4610b06599ee2179f76e';
-const String spotifyClientSecret = '9745aada719e42daaac2d1d2c525e6e6';
+const String spotifyClientId = 'your_key';
+const String spotifyClientSecret = 'your_key';
 
 /// Position to decide when a track is stored (for recommendation) to storage.
 const double positionToStoreTrack = 0.8;
